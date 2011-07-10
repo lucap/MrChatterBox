2c268af0335468e6bec64d995fe2fb05e47260ca
Luca Pellicoro <lpellicoro@playdom.com>
2011-07-09 22:11:49 -0700

    Use data structure to hold list of clients

--- server.py
+++ server.py
@@ -10,44 +10,92 @@ import tornadio
 import tornadio.router
 import tornadio.server
 
-
 ROOT = op.normpath(op.dirname(__file__))
+        
+class ClientCollection:
+    
+    @classmethod
+    def instance(cls):
+        if not hasattr(cls, "_instance"):
+            cls._instance = cls()
+        return cls._instance
 
-participants = []
-
-
-class IndexHandler(tornado.web.RequestHandler):
+    @classmethod
+    def initialized(cls):
+        return hasattr(cls, "_instance")
+    
+    def __init__(self):
+        self.users = {}
+    
+    def add(self, name, client):
+        success = False
+        if name not in self.users:
+            self.users[name] = client
+            success = True
+            
+        return success
+    
+    def remove(self, name):
+        if name in self.users:
+            del self.users[name]    
+        
+    def get_clients(self, omitt_name):
+        clients = []
+        for name, client in self.users.iteritems():
+            if name != omitt_name:
+                clients.append((name, client))
+        return clients
+        
+        
+class ChatRoomHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("index.html", user_id=len(participants))
+        username = self.get_argument("username", default="not-set", strip=True)
+        self.render("chatroom.html", username=username)
+        
+        
+class PortalHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("portal.html")
+
 
 class SocketIOConnection(tornadio.SocketConnection):
 
+    def __init__(self, *args, **kwargs):
+        tornadio.SocketConnection.__init__(self, *args, **kwargs)
+        self.username = None
+
     def on_open(self, *args, **kwargs):
         print "New Client"
-        
-        for i in range(len(participants)):
-            print i
-            message = ["new_client", i]
-            self.send(simplejson.dumps(message))
-        
-        message = ["new_client", len(participants)]
-        for p in participants:
-            p.send(simplejson.dumps(message))
-            
-        participants.append(self)
 
     def on_close(self):
-        message = ["remove_client", participants.index(self)]
-        participants.remove(self)
-        for p in participants:
-            p.send(simplejson.dumps(message))
+        client_collection = ClientCollection.instance()
+        clients = client_collection.get_clients(self.username)
+        for name, client in clients:
+            client.send({"remove_client": self.username})
+        
+        client_collection.remove(self.username)
         
     def on_message(self, message):
-        user_id, text = message.split(" ", 1)
-        for p in participants:
-            if p != self:
-                p.send(simplejson.dumps([user_id, text]))
-
+        print message
+        client_collection = ClientCollection.instance()
+        
+        if "new_user" in message:
+            username = message["new_user"]
+            client_collection.add(username, self)
+            clients = client_collection.get_clients(username)
+            
+            for name, client in clients:
+                # Add existing clients to this current one
+                self.send({"new_client": name})
+                
+                # Send the the current new client to all existing ones
+                client.send({"new_client": username})
+            self.username = username
+            
+        elif "new_message" in message and self.username:
+            clients = client_collection.get_clients(self.username)
+            for name, client in clients:
+                client.send(message)
 
 if __name__ == "__main__":
     import logging
@@ -56,7 +104,10 @@ if __name__ == "__main__":
 
     #configure the Tornado application
     application = tornado.web.Application(
-        [(r"/", IndexHandler), SocketIOConnectionRouter.route()],
+        [(r"/", PortalHandler),
+        (r"/portal.html", PortalHandler),
+        (r"/chatroom.html", ChatRoomHandler),
+        SocketIOConnectionRouter.route()],
         static_path = op.join(ROOT, "static"),
         enabled_protocols = ['websocket',
                              'flashsocket',
